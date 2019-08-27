cddc8600598fbf7a6daf8dfb9fc4f67e47613e4b
Glitch (florentine-corn) <none>
2019-08-27 20:22:32 +0000

    💘💎 Checkpoint ./server.js:6099450/235

--- server.js
+++ server.js
@@ -20,8 +20,14 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/API/timestamp/:date_string?", function (req, res) {
+  
+  if (!req.params.date_string){
+  //console.log('empty')
+    return res.json({data: new Date()})
+}
+  console.log(req.params.date_string)
+  res.json({greeting: req.params.date_string});
 });
 
 
